7a30ec5c6ea191aa4cd83db6baafb75c1bb51df7
M1kanN <fangliu2896@gmail.com>
2023-11-29 00:05:49 +0900

    day8: vulkan tutorial 16 - index and staging buffer

--- include/m1k_model.hpp
+++ include/m1k_model.hpp
@@ -23,8 +23,13 @@ class M1kModel {
         static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
     };
 
+    struct Builder {
+        std::vector<Vertex> vertices{};
+        std::vector<uint32_t> indices{};
+    };
 
-    M1kModel(M1kDevice& device, const std::vector<Vertex>& vertices);
+
+    M1kModel(M1kDevice& device, const M1kModel::Builder &builder);
     ~M1kModel();
 
     M1kModel(const M1kModel&) = delete;
@@ -35,11 +40,18 @@ class M1kModel {
 
    private:
     void createVertexBuffers(const std::vector<Vertex> &vertices);
+    void createIndexBuffers(const std::vector<uint32_t> &indices);
 
     M1kDevice& m1K_device_;
+
     VkBuffer vertex_buffer_;
     VkDeviceMemory vertex_buffer_memory_;
     uint32_t vertex_count_;
+
+    bool has_index_buffer_{false};
+    VkBuffer index_buffer_;
+    VkDeviceMemory index_buffer_memory_;
+    uint32_t index_count_;
 };
 
 }
--- m1k_model.cpp
+++ m1k_model.cpp
@@ -10,13 +10,19 @@
 
 namespace m1k {
 
-M1kModel::M1kModel(M1kDevice& device, const std::vector<Vertex>& vertices) : m1K_device_(device) {
-    createVertexBuffers(vertices);
+M1kModel::M1kModel(M1kDevice& device, const M1kModel::Builder &builder) : m1K_device_(device) {
+    createVertexBuffers(builder.vertices);
+    createIndexBuffers(builder.indices);
 }
 
 M1kModel::~M1kModel() {
     vkDestroyBuffer(m1K_device_.device(), vertex_buffer_, nullptr);
     vkFreeMemory(m1K_device_.device(), vertex_buffer_memory_, nullptr);
+
+    if(has_index_buffer_) {
+        vkDestroyBuffer(m1K_device_.device(), index_buffer_, nullptr);
+        vkFreeMemory(m1K_device_.device(), index_buffer_memory_, nullptr);
+    }
 }
 
 void M1kModel::createVertexBuffers(const std::vector<Vertex> &vertices) {
@@ -24,27 +30,88 @@ void M1kModel::createVertexBuffers(const std::vector<Vertex> &vertices) {
     assert(vertex_count_ >= 3 && "Vertex count must be at least 3");
     VkDeviceSize buffer_size = sizeof(vertices[0]) * vertex_count_;
 
+    // staging buffer is good for static data
+    VkBuffer staging_buffer;
+    VkDeviceMemory staging_buffer_memory;
     m1K_device_.createBuffer(
         buffer_size,
-        VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+        VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
         VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+        staging_buffer,
+        staging_buffer_memory);
+
+    void *data;
+    vkMapMemory(m1K_device_.device(), staging_buffer_memory, 0, buffer_size, 0, &data);
+    memcpy(data, vertices.data(), static_cast<uint32_t>(buffer_size));
+    vkUnmapMemory(m1K_device_.device(), staging_buffer_memory);
+
+    m1K_device_.createBuffer(
+        buffer_size,
+        VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+        VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
         vertex_buffer_,
         vertex_buffer_memory_);
 
+    m1K_device_.copyBuffer(staging_buffer, vertex_buffer_, buffer_size);
+
+    vkDestroyBuffer(m1K_device_.device(), staging_buffer, nullptr);
+    vkFreeMemory(m1K_device_.device(), staging_buffer_memory, nullptr);
+}
+
+void M1kModel::createIndexBuffers(const std::vector<uint32_t> &indices) {
+    index_count_ = static_cast<uint32_t>(indices.size());
+    has_index_buffer_ = index_count_ > 0;
+    if(!has_index_buffer_) {
+        return;
+    }
+
+    assert(index_count_ >= 3 && "Index count must be at least 3");
+    VkDeviceSize buffer_size = sizeof(indices[0]) * index_count_;
+
+    // staging buffer is good for static data
+    VkBuffer staging_buffer;
+    VkDeviceMemory staging_buffer_memory;
+    m1K_device_.createBuffer(
+        buffer_size,
+        VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+        VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+        staging_buffer,
+        staging_buffer_memory);
+
     void *data;
-    vkMapMemory(m1K_device_.device(), vertex_buffer_memory_, 0, buffer_size, 0, &data);
-    memcpy(data, vertices.data(), static_cast<uint32_t>(buffer_size));
-    vkUnmapMemory(m1K_device_.device(), vertex_buffer_memory_);
+    vkMapMemory(m1K_device_.device(), staging_buffer_memory, 0, buffer_size, 0, &data);
+    memcpy(data, indices.data(), static_cast<size_t>(buffer_size));
+    vkUnmapMemory(m1K_device_.device(), staging_buffer_memory);
+
+    m1K_device_.createBuffer(
+        buffer_size,
+        VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+        VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+        index_buffer_,
+        index_buffer_memory_);
+
+    m1K_device_.copyBuffer(staging_buffer, index_buffer_, buffer_size);
+
+    vkDestroyBuffer(m1K_device_.device(), staging_buffer, nullptr);
+    vkFreeMemory(m1K_device_.device(), staging_buffer_memory, nullptr);
 }
 
 void M1kModel::bind(VkCommandBuffer command_buffer) {
     VkBuffer buffers[] = {vertex_buffer_};
     VkDeviceSize offsets[] = {0};
     vkCmdBindVertexBuffers(command_buffer, 0, 1, buffers, offsets);
+
+    if(has_index_buffer_) {
+        vkCmdBindIndexBuffer(command_buffer, index_buffer_, 0, VK_INDEX_TYPE_UINT32);
+    }
 }
 
 void M1kModel::draw(VkCommandBuffer command_buffer) {
-    vkCmdDraw(command_buffer, vertex_count_, 1, 0, 0);
+    if(has_index_buffer_) {
+        vkCmdDrawIndexed(command_buffer, index_count_, 1, 0, 0, 0);
+    } else {
+        vkCmdDraw(command_buffer, vertex_count_, 1, 0, 0);
+    }
 }
 
 std::vector<VkVertexInputBindingDescription> M1kModel::Vertex::getBindingDescriptions() {
